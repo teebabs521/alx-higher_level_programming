8e2c6365c66238811e502503018757cf50631313
Ibrahim <teebabs52@gmail.com>
2024-10-16 02:56:40 -0700

    Second commit

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,20 +1,40 @@
-!/usr/bin/python3
-'''Module for Rectangle class.'''
-Rectangle = __import__('9-rectangle').Rectangle
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 29 15:13:37 2020
+
+@author: Robinson Montes
+"""
 
 
-class Square(Rectangle):
-    '''A subclass representing a rectangle.'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(BaseGeometry):
+    """
+    A Square class shape, inheirts from BaseGeometry
+    """
     def __init__(self, size):
-        '''Constructor.'''
+        """"
+        Init function for Square
+
+        Attributes:
+            size (int): The size of the square
+        """
         self.integer_validator("size", size)
         self.__size = size
-        super().__init__(size, size)
-
-    def area(self):
-        '''Method for area of square.'''
-        return self.__size ** 2
 
     def __str__(self):
-        '''Returns string representation of this square.'''
-        return "[Square] " + str(self.__size) + "/" + str(self.__size)
+        """
+        str funtion to print with/height
+
+        Returns:
+            Return width/height
+        """
+        return '[Square] ' + str(self.__size) + '/' + str(self.__size)
+
+    def area(self):
+        """
+        A function that calculates the area of the Square
+        """
+        return self.__size ** 2
